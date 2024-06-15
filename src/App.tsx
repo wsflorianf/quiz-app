6ec8247a757cfefb9a71526ebbe650d9ea83f6e7
Santiago Florian <floranwsfrench@gmail.com>
2024-06-15 10:36:37 -0500

    Estructura inicial y modo oscuro

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, FluentProvider, Title1 } from '@fluentui/react-components'
+import { lightTheme, darkTheme } from './utils/themes'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [darkMode, setDarkMode] = useState(false)
+
+  const changeDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
+
+  const mainTheme = darkMode ? darkTheme : lightTheme
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <FluentProvider theme={mainTheme}>
+      <div id='main-container'>
+        <nav style={{boxShadow: mainTheme.shadow2}}>
+          <div></div>
+          <Title1>Quiz App</Title1>
+          <Button
+            onClick={changeDarkMode}
+            shape='circular'
+            icon={<>{darkMode?' 🌞 ':' 🌝 '}</>}
+          ></Button>
+        </nav>
+        <div></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </FluentProvider>
   )
 }
 
