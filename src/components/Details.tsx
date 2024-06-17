8194699b0800f45e6ad659539094ba7783e9fcd3
Santiago Florian <floranwsfrench@gmail.com>
2024-06-17 17:58:56 -0500

    Iconos cambiados con estilos

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,19 +1,51 @@
-import { Button } from "@fluentui/react-components"
-import useQuestions from "../store/useQuestions"
+import { Button, tokens } from '@fluentui/react-components'
+import useQuestions from '../store/useQuestions'
+import {
+  CheckmarkSquare24Filled,
+  DismissSquare24Filled,
+  QuestionCircle24Filled,
+} from '@fluentui/react-icons'
 
-export default function Details (){
+export default function Details() {
+  const questions = useQuestions((state) => state.questions)
+  const resetQuestions = useQuestions((state) => state.resetQuestions)
 
-    const questions = useQuestions(state=>state.questions)
-    const resetQuestions = useQuestions(state=>state.resetQuestions)
-
-    return (
-        <>
-        <div id="info">
-            <div>✅ Correctas: {questions.reduce((sum, question)=>question.correct===question.selected?sum+1:sum,0)}</div>
-            <div>❌ Incorretas: {questions.reduce((sum, question)=>(question.selected!==undefined && question.selected!==question.correct)?sum+1:sum,0)}</div>
-            <div>❓ Sin responder: {questions.reduce((sum, question)=>(question.selected===undefined)?sum+1:sum,0)}</div>
+  return (
+    <>
+      <div id='info'>
+        <div>
+          <CheckmarkSquare24Filled color={tokens.colorStatusSuccessBackground3} /> Correctas:{' '}
+          {questions.reduce(
+            (sum, question) =>
+              question.correct === question.selected ? sum + 1 : sum,
+            0
+          )}
         </div>
-        {questions.some((question)=>(question.selected===undefined))?'':<Button onClick={resetQuestions}>Volver a comenzar</Button>}
-        </>
-    )
+        <div>
+          <DismissSquare24Filled color={tokens.colorStatusDangerBackground3}/> Incorretas:{' '}
+          {questions.reduce(
+            (sum, question) =>
+              question.selected !== undefined &&
+              question.selected !== question.correct
+                ? sum + 1
+                : sum,
+            0
+          )}
+        </div>
+        <div>
+          <QuestionCircle24Filled color={tokens.colorPaletteRoyalBlueBorderActive}/> Sin responder:{' '}
+          {questions.reduce(
+            (sum, question) =>
+              question.selected === undefined ? sum + 1 : sum,
+            0
+          )}
+        </div>
+      </div>
+      {questions.some((question) => question.selected === undefined) ? (
+        ''
+      ) : (
+        <Button onClick={resetQuestions}>Volver a comenzar</Button>
+      )}
+    </>
+  )
 }
