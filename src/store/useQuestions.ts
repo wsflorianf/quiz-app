0253fee90a8b643220281df54a943bc685a0f472
Santiago Florian <floranwsfrench@gmail.com>
2024-06-17 17:03:14 -0500

    Métodos para gestionar el cambio de preguntas, selección de respuestas y reinicio

--- src/store/useQuestions.ts
+++ src/store/useQuestions.ts
@@ -5,17 +5,31 @@ import { Question } from "../types/question";
 interface QuestionsTypes{
     questions: Question[]
     currentQuestion: number
-    getQuestions: ()=>void
+    getQuestions:  ()=>void
+    prevQuestion: ()=>void
+    nextQuestion: ()=>void
+    selectAnswer: (answer: number)=>void
+    resetQuestions: ()=>void
 
 }
 
 
-const useQuestions = create<QuestionsTypes>()(()=>({
+const useQuestions = create<QuestionsTypes>()((set)=>({
     questions: [],
     currentQuestion: 0,
-    getQuestions: ()=>{
-
-    }
+    getQuestions: async ()=>{
+        const myQuestions: Question[] = await fetch('http://localhost:5173/src/utils/questions.json').then(response => response.json())
+        set({questions: myQuestions})
+    },
+    prevQuestion: ()=>set((state)=>{if(state.currentQuestion>0){return {currentQuestion: state.currentQuestion-1}} else {return {}}}),
+    nextQuestion: ()=>set((state)=>{if(state.currentQuestion<state.questions.length-1){return {currentQuestion: state.currentQuestion+1}} else {return {}}}),
+    selectAnswer: (answer)=>set((state)=>{
+        const newQuestions = [...state.questions]
+        newQuestions[state.currentQuestion].selected = answer
+        
+        return {questions: newQuestions}
+    }),
+    resetQuestions: ()=>set({questions: []})
 }))
 
 export default useQuestions
