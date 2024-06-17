e9a087c6d11e53003e610a53a6622904d3556491
Santiago Florian <floranwsfrench@gmail.com>
2024-06-17 17:01:58 -0500

    Añadida lógica para elegir preguntas y reiniciar

---
+++ src/components/QuizGame.tsx
@@ -0,0 +1,83 @@
+import {
+  Button,
+  MenuItem,
+  MenuList,
+  Subtitle2,
+  tokens,
+} from '@fluentui/react-components'
+import { ArrowLeft32Regular, ArrowRight32Regular } from '@fluentui/react-icons'
+import useQuestions from '../store/useQuestions'
+import useDarkMode from '../store/useDarkMode'
+import Details from './Details'
+
+export default function QuizGame() {
+  const darkMode = useDarkMode((state) => state.darkMode)
+  const questions = useQuestions((state) => state.questions)
+  const currentQuestion = useQuestions((state) => state.currentQuestion)
+  const prevQuestion = useQuestions((state) => state.prevQuestion)
+  const nextQuestion = useQuestions((state) => state.nextQuestion)
+  const selectAnswer = useQuestions((state) => state.selectAnswer)
+
+  const getBackgroundColor = (index: number) => {
+    const selected = questions[currentQuestion].selected
+
+    if (selected !== undefined) {
+      if (questions[currentQuestion].correct == selected && selected == index) {
+        return tokens.colorStatusSuccessBackground2
+      } else {
+        if (selected == index) {
+          return tokens.colorStatusDangerBackground2
+        }
+        if (questions[currentQuestion].correct == index) {
+          return tokens.colorStatusSuccessBackground2
+        }
+      }
+    }
+  }
+
+  return (
+    <>
+    <div id='quiz-game' className={darkMode ? 'dark' : ''}>
+      <div id='controls'>
+        <Button
+          disabled={currentQuestion <= 0}
+          appearance='outline'
+          size='large'
+          icon={<ArrowLeft32Regular />}
+          onClick={prevQuestion}
+        />
+        <Subtitle2>{`${currentQuestion + 1} / ${questions.length}`}</Subtitle2>
+        <Button
+          disabled={currentQuestion + 1 >= questions.length}
+          appearance='outline'
+          size='large'
+          icon={<ArrowRight32Regular />}
+          onClick={nextQuestion}
+        />
+      </div>
+      <div
+        id='question'
+        style={{ backgroundColor: tokens.colorNeutralBackgroundAlpha }}
+      >
+        {questions[currentQuestion]?.question}
+      </div>
+      <MenuList id='answers'>
+        {questions[currentQuestion]?.answers.map((answer, index) => {
+          return (
+            
+            <MenuItem
+                disabled={questions[currentQuestion]?.selected!==undefined}
+              onClick={() => selectAnswer(index)}
+              style={{ backgroundColor: getBackgroundColor(index) }}
+              key={index}
+            >
+              {answer}
+            </MenuItem>
+          )
+        })}
+      </MenuList>
+    </div>
+    <Details/>
+    </>
+  )
+}
